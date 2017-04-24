d7b904bdbd73447c3f0c7ec9d0bcf0eb2df26ffc
huos3203 <724987481@qq.com>
2017-04-24 22:56:27 +0800

    init

---
+++ BDDExamplesSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BDDExamplesSwift
+//
+//  Created by huoshuguang on 2017/4/24.
+//  Copyright © 2017年 huoshuguang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
